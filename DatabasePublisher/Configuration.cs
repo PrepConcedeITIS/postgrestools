eab0dfe4bd8a2b35e3b92e404ca511b5090d300d
kirill <kirillaromad@mail.ru>
2024-04-22 23:42:49 +0300

    add configutaion class

---
+++ DatabasePublisher/Configuration.cs
@@ -0,0 +1,12 @@
+﻿namespace DatabasePublisher;
+
+public class Configuration
+{
+    public required string TempConnectionString { get; set; }
+    public required string TargetConnectionString { get; set; }
+    public required string SchemaDirectory { get; set; }
+    public required string OutputDirectory { get; set; }
+    public bool GeneratePublishFile { get; set; }
+    public bool Debug { get; set; }
+    public bool DoNotDrop { get; set; }
+}
